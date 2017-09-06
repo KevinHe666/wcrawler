6fe5d93d2f25fe2474c06f8b1dafd7ccf559854b
wuyi5 <ngone_5451@163.com>
2017-09-06 18:39:54 +0800

    增加代理site

--- src/main/java/com/wuyi/wcrawler/proxy/util/ProxySite.java
+++ src/main/java/com/wuyi/wcrawler/proxy/util/ProxySite.java
@@ -8,7 +8,12 @@ public enum ProxySite {
 	XICIDAILI(XicidailiParser.class, "http://www.xicidaili.com", new String[]{"nn", "nt", "wn", "wt"}),
 	IP181(Ip181Parser.class, "http://www.ip181.com", null),
 	MIMIIP(MimiParser.class, "http://www.mimiip.com", new String[]{"gngao", "gnpu"});
-
+//	http://www.kuaidaili.com/free/
+//	http://www.66ip.cn/
+//	http://www.ip3366.net/free/
+//	http://www.proxy360.cn/Region/China
+//	http://www.data5u.com/free/index.shtml
+//	http://www.kxdaili.com/
 	private Class<?> clazz;
 	private String site;
 	private String[] domains;
